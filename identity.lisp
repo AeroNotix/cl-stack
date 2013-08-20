23de40c2f049d75be97a477a975ecaec195d5676
Aaron <aaron.l.france@gmail.com>
2013-08-20 21:32:13 +0200

    Added a defclass for an openstack-client which will populate some fields with the access-token and the expires value.

--- identity.lisp
+++ identity.lisp
@@ -5,6 +5,10 @@
 
 (defvar identurl "https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/")
 
+(defclass openstack-client ()
+  ((access-token :initarg :access-token)
+   (expires :initarg :expires)))
+
 (defun make-auth-hash (username password tenantid)
   (let ((auth (make-hash-table :test #'equal))
         (sub-auth (make-hash-table :test #'equal))
@@ -19,10 +23,21 @@
 (defun handle-api-error (code)
   (print code))
 
+(defun make-client-from-response (response)
+  (make-instance 'openstack-client
+		 :access-token (drill-hash response '("access" "token" "id"))
+		 :expires (drill-hash response '("access" "token" "expires"))))
+
 (defun login (username password tenantid)
-  (let ((auth (make-auth-hash username password tenantid)))
-    (drakma:http-request (format nil "~atokens" identurl)
+  (let* ((auth (make-auth-hash username password tenantid))
+	 (resp (multiple-value-list (drakma:http-request (format nil "~atokens" identurl)
                          :method :post
                          :content (encode-hash auth)
                          :content-type "application/json"
-                         :additional-headers '(("Accept" . "application/json")))))
+                         :additional-headers '(("Accept" . "application/json"))
+			 :want-stream T)))
+	 (body (nth 0 resp))
+	 (status-code (nth 1 resp)))
+    (if (= status-code 200)
+	(make-client-from-response (yason:parse body))
+	(handle-api-error status-code))))
