94b678d396c0cfb07a6494332697a69a9ce945ac
AeroNotix <aaron.l.france@gmail.com>
2013-08-20 16:36:50 +0100

    Got logins working.

--- identity.lisp
+++ identity.lisp
@@ -1,6 +1,26 @@
 (in-package :cl-stack-identity)
 
-(defun login (username password)
-  (drakma:http-request "https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/"
-		       :method :post
-		       :content))
+(defvar identurl "https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/")
+
+(defun make-auth-hash (username password tenantid)
+  (let ((auth (make-hash-table :test #'equal))
+        (sub-auth (make-hash-table :test #'equal))
+        (pass-creds (make-hash-table :test #'equal)))
+    (setf (gethash "auth" auth) sub-auth
+          (gethash "passwordCredentials" sub-auth) pass-creds
+          (gethash "tenantId" sub-auth) tenantid
+          (gethash "username" pass-creds) username
+          (gethash "password" pass-creds) password)
+    auth))
+
+(defun encode-hash (hash)
+  (with-output-to-string (*standard-output*)
+    (yason:encode hash)))
+
+(defun login (username password tenantid)
+  (let ((auth (make-auth-hash username password tenantid)))
+    (drakma:http-request (format nil "~atokens" identurl)
+                         :method :post
+                         :content (encode-hash auth)
+                         :content-type "application/json"
+                         :additional-headers '(("Accept" . "application/json")))))
