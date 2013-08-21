5d357d5c326b704f1fba0b15933f507b70f9ff64
AeroNotix <aaron.l.france@gmail.com>
2013-08-21 16:55:18 +0100

    Export the md5 related functions from the utils package.

--- packages.lisp
+++ packages.lisp
@@ -1,9 +1,20 @@
 (in-package :cl-user)
 
-(defpackage #:cl-stack-identity
-  (:use #:cl #:cl-stack-utils)
-  (:export #:login))
-
 (defpackage #:cl-stack-utils
   (:use #:cl)
-  (:export #:drill-hash #:encode-hash))
+  (:export #:drill-hash
+           #:encode-hash
+           #:md5-digest
+           #:md5-digest-file))
+
+(defpackage #:cl-stack-identity
+  (:use #:cl #:cl-stack-utils)
+  (:export #:login
+           #:openstack-client
+           #:tenantid))
+
+(defpackage #:cl-stack-objectstore
+  (:use #:cl
+        #:cl-stack-utils
+        #:cl-stack-identity)
+  (:export #:upload-file))
