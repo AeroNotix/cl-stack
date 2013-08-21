4e394e01ab02da3fd46342413b06cd918847fb6a
AeroNotix <aaron.l.france@gmail.com>
2013-08-21 16:25:46 +0100

    Added the md5 functions to the utils.lisp package.

--- utils.lisp
+++ utils.lisp
@@ -1,10 +1,17 @@
 (in-package :cl-stack-utils)
 
 (ql:quickload :yason)
+(ql:quickload :sb-md5)
 
 (defun encode-hash (hash)
   (with-output-to-string (*standard-output*)
     (yason:encode hash)))
+
+(defun md5-digest (input)
+  (format nil "~{~2,'0x~}" input))
+
+(defun md5-digest-file (filename)
+  (md5-digest (coerce (sb-md5:md5sum-file filename) 'list)))
 
 (defun drill-hash (hash keys)
   (reduce #'(lambda (a b) (gethash b a)) keys :initial-value hash))
