eef76285e8934de2924813033a9871b9a18ad35e
Aaron <aaron.l.france@gmail.com>
2013-08-21 23:48:03 +0200

    Uploading files now works but we're not doing anything about the ETag MD5.

--- objectstore.lisp
+++ objectstore.lisp
@@ -4,18 +4,17 @@
 (ql:quickload :yason)
 (ql:quickload :sb-md5)
 
-(defvar computeurl "https://region-b.geo-1.objects.hpcloudsvc.com/v1.0/")
+(defvar computeurl "https://region-b.geo-1.objects.hpcloudsvc.com/v1/")
 
 (defmethod upload-file ((client openstack-client) (filename string)
-                        &key (container "") (content-type "application/text") headers)
-  (let* ((md5hash (md5-digest-file filename))
-         (url (format nil "~a~a~a/~a"
+                        &key (container "/testainer") (content-type "application/text") headers)
+  (let* ((url (format nil "~a~a~a/~a"
                       computeurl
                       (slot-value client 'tenantid)
                       container
                       (pathname-name filename)))
          (auth-token (slot-value client 'access-token))
-         (req-headers `(("X-Auth-Token" ,auth-token) ("ETag" ,md5hash)))
+         (req-headers `(("X-Auth-Token" . ,auth-token)))
          (headers (append headers req-headers)))
     (drakma:http-request url :method :POST
                              :content-type content-type
