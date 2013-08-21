839c8702a92a7e43f9a4cb8f39cced42d9c3988e
AeroNotix <aaron.l.france@gmail.com>
2013-08-21 18:07:09 +0100

    We are now making the request to the OpenStack server for uploading files.

--- objectstore.lisp
+++ objectstore.lisp
@@ -4,10 +4,20 @@
 (ql:quickload :yason)
 (ql:quickload :sb-md5)
 
-(defvar computeurl "https://region-b.geo-1.objects.hpcloudsvc.com:443/v1/")
+(defvar computeurl "https://region-b.geo-1.objects.hpcloudsvc.com/v1.0/")
 
 (defmethod upload-file ((client openstack-client) (filename string)
-                        &key path content-type headers)
-  (let ((md5hash (md5-digest-file filename))
-        (url (format nil "~a~a/~a" computeurl (slot-value client 'tenantid) filename)))
-    (print (list url path content-type headers md5hash))))
+                        &key (container "/") (content-type "application/text") headers)
+  (let* ((md5hash (md5-digest-file filename))
+         (url (format nil "~a~a~a/~a"
+                      computeurl
+                      (slot-value client 'tenantid)
+                      container
+                      (pathname-name filename)))
+         (auth-token (slot-value client 'access-token))
+         (req-headers `(("X-Auth-Token" ,auth-token) ("ETag" ,md5hash)))
+         (headers (append headers req-headers)))
+    (drakma:http-request url :method :POST
+                             :content-type content-type
+                             :content (open filename)
+                             :additional-headers headers)))
