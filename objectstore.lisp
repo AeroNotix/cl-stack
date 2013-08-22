cc421c26f3169e4179c8cc1c5a77628dae62f0bf
Aaron <aaron.l.france@gmail.com>
2013-08-22 21:57:45 +0200

    Added todos and docstrings.

--- objectstore.lisp
+++ objectstore.lisp
@@ -2,11 +2,22 @@
 
 (ql:quickload :drakma)
 (ql:quickload :yason)
-(ql:quickload :sb-md5)
+(ql:quickload :sb-md5) ;; TODO: Find a implementation agnostic MD5
+		       ;; library.
 
+
+;; TODO: Have this come from a configuration file to allow for
+;; OpenStack vendor agnostic URLs.
 (defvar computeurl "https://region-b.geo-1.objects.hpcloudsvc.com/v1/")
 
 (defmethod base-headers ((client openstack-client) &optional (additional nil additional-p))
+  "Each request has a set of basic headers which are required in each
+  and every request we send to the OpenStack vendor.
+
+  These include:
+      * X-Auth-Token: An authentication token which we receive after
+        logging in.
+      * Accept: We always accept application/json"
   (let* ((auth-token (slot-value client 'access-token))
          (req-headers `(("X-Auth-Token" . ,auth-token)))
          (headers (if additional-p
@@ -16,6 +27,7 @@
 
 (defmethod upload-file ((client openstack-client) (filename string)
                         &key (container "/testainer") (content-type "application/text") headers)
+  "Upload file will upload `filename' into the ObjectStore."
   (let* ((url (format nil "~a~a~a/~a"
                       computeurl
                       (slot-value client 'tenantid)
@@ -36,6 +48,7 @@
         status-code)))
 
 (defmethod remove-file ((client openstack-client) (filename string))
+  "Remove file will remove the `filename' from the ObjectStore."
   (let* ((url (format nil "~a~a~a"
                       computeurl
                       (slot-value client 'tenantid)
@@ -47,6 +60,8 @@
     (if (= status-code 204) T status-code)))
 
 (defmethod create-directory ((client openstack-client) (directory string) &key headers)
+  "Create directory will create the `directory' in the ObjectStore as
+  a basic container."
   (let* ((url (format nil "~a~a~a"
                       computeurl
                       (slot-value client 'tenantid)
